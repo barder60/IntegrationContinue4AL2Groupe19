fd4e0085bbf24791cf399b39269d34dd7975f722
barder60 <kaderate.yamamoto@hotmail.fr>
2021-02-07 16:54:59 +0100

    test(test): adding test jest & coverage

---
+++ test/index.test.js
@@ -0,0 +1,23 @@
+const supertest = require("supertest")
+
+const startServer = require('../src/infrastructure/webServerFramework/startApp')
+
+jest.setTimeout(60000)
+
+describe('route : /', () => {
+    let request
+
+    beforeAll(async done => {
+        const app = await startServer()
+
+        request = supertest(app)
+        done()
+    })
+
+    it('should return {wel:"come"}', async (done) => {
+        const res = await request.get('/')
+
+        expect(res.body).toEqual({wel:"come"})
+        done()
+    })
+})
